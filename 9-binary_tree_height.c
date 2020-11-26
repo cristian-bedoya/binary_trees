ae2fc17a1d58fc0c408cb511b86e156d4204b8b6
cristian <crisbed321@gmail.com>
2020-11-26 20:25:38 +0000

    add task 9 again

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -7,7 +7,7 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t left_height = 0; 
+	size_t left_height = 0;
 	size_t right_height = 0;
 
 	if (!tree || (!(tree->right) && !(tree->right)))
