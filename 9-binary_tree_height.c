d888e32feb84e9441b3444321ad8b54f00ade382
cristian <crisbed321@gmail.com>
2020-11-26 20:15:45 +0000

    updated 9 task

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -13,13 +13,13 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	if (!tree || (!(tree->right) && !(tree->right)))
 		return (0);
 
-	left_height = binary_tree_height(tree->left);
-	right_height = binary_tree_height(tree->right);
+	left_height = binary_tree_height(tree->left) + 1;
+	right_height = binary_tree_height(tree->right) + 1;
 
 	if (left_height > right_height)
 		height = left_height;
 	else
 		height = right_height;
-	return (height + 1);
+	return (height);
 
 }
