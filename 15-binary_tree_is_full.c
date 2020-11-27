d4cab71f5383bcbb579f2d7251e28f3268c8c4f8
cristian <crisbed321@gmail.com>
2020-11-27 00:24:34 +0000

    hot fix

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -11,7 +11,7 @@ int binary_tree_is_full(const binary_tree_t *tree)
 	if (!tree)
 		return (0);
 
-	if (tree->left == NULL && tree->right == NULL) 
+	if (!(tree->left) && !(tree->right))
 		return (1);
 
 	if ((tree->left) && (tree->right))
